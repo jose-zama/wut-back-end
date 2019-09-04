0dd3bf8c9c2f2e2479a1842e1cd389291008dc36
LBrito <ing.l.brito@gmail.com>
2019-09-04 01:43:17 -0700

    Added JSON Validation

--- App/server.py
+++ App/server.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import request, jsonify
+from flask_json_schema import JsonSchema, JsonValidationError
 from ..Conf.container import Controllers
+from .validation import check_movements_schema
 
 
 app = Flask(__name__)
+schema = JsonSchema(app)
 app_controller = Controllers.app()
 
 
@@ -13,13 +16,18 @@ def health_check():
 
 
 @app.route('/classification/movements', methods=['POST'])
+@schema.validate(check_movements_schema)
 def check_movements():
-    # TODO: Validate input
     result = app_controller.check_movements(request.get_json())
     return jsonify(result)
-
 
 @app.errorhandler(404)
 def error_handler(error):
     return '404 Not Found', 404
+
+
+@app.errorhandler(JsonValidationError)
+def validation_error(e):
+    return jsonify({'error': e.message, 'errors': [error.message for error in e.errors]}), 400
+
 
---
+++ App/validation.py
@@ -0,0 +1,15 @@
+
+check_movements_schema = {
+  "type": "object",
+  "required": [
+    "movements"
+  ],
+  "properties": {
+    "movements": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      }
+    }
+  }
+}
