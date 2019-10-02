474206009287395423f3f87354124d6fa8e96262
LBrito <ing.l.brito@gmail.com>
2019-10-01 19:11:57 -0700

    Added Pytorch test

--- Adapter/ML/pytorch_classification_algorithm.py
+++ Adapter/ML/pytorch_classification_algorithm.py
@@ -1,12 +1,23 @@
-from ...Usecase.ML.classification_algorithm import ClassificationAlgorithm
+from ...Usecase.ML import ClassificationAlgorithm
+from spacy.lang.en.examples import sentences
+from nltk.corpus import stopwords
+from torchtext import data
+
+import spacy
+import nltk
+import re
+import torchtext
+import torch.nn as nn
+import torch
+import numpy as np
 import pandas as pd
 
 
 class PytorchClassificationAlgorithm(ClassificationAlgorithm):
 
     def classify(self, movements):
-        df = pd.DataFrame(movements)
-        # Call Pytorch
-        return [
-            {"classification": "Food & Dining", "movement": "8284120518LOAC720417H51CAFE DAS CORTEZ I"}
-        ]
+        return {
+            "Movements": [
+                {"Category": "Entertainment", "Movement": "Cheves en la azotea"}
+            ]
+        }
